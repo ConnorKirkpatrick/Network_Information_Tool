5cff89ee9fb30377a6f2b7fa08fc53e7ef822ba8
Connor <ConnorKirkpatrick3@gmail.com>
2022-08-09 00:19:58 +0100

    Created test case for new sqlite connection

---
+++ sqlTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SQLite;
+using NUnit.Framework;
+
+namespace Network_Tool
+{
+    public class SqlTests
+    {
+        [Test]
+        public void connectionTest()
+        {
+            SQLiteConnection conn = new SQLiteConnection("Data Source=D:\\Network_Information_Tool\\testData.sql;FailIfMissing=True;");
+            conn.Open();
+            Assert.AreEqual(conn.State.ToString(), "Open","Open connection not established");
+            conn.Close();
+        }
+    }
+}

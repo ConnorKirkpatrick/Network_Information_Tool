bf28991e2defb84896936566292aea342b294d22
Connor <ConnorKirkpatrick3@gmail.com>
2022-08-09 17:53:33 +0100

    Added network hop object and tests to replace reliance on lists

---
+++ NetworkingMethods/NetworkHop.cs
@@ -0,0 +1,69 @@
+﻿using System.Net;
+
+namespace Network_Tool.NetworkingMethods
+{
+    public class NetworkHop
+    {
+        private IPAddress ipv4Address;
+        private int sequenceNumber;
+        private int latency;
+        private int packetLoss;
+        private NetworkHop previousNode;
+        private NetworkHop nextNode;
+        public IPAddress Ipv4Address
+        {
+            get => ipv4Address;
+            set => ipv4Address = value;
+        }
+
+        public int SequenceNumber
+        {
+            get => sequenceNumber;
+            set => sequenceNumber = value;
+        }
+
+        public int Latency
+        {
+            get => latency;
+            set => latency = value;
+        }
+
+        public int PacketLoss
+        {
+            get => packetLoss;
+            set => packetLoss = value;
+        }
+        
+        public NetworkHop PreviousNode
+        {
+            get => previousNode;
+            set => previousNode = value;
+        }
+
+        public NetworkHop NextNode
+        {
+            get => nextNode;
+            set => nextNode = value;
+        }
+        
+        public NetworkHop(IPAddress ipv4Address, int sequenceNumber, int latency, int packetLoss)
+        {
+            this.ipv4Address = ipv4Address;
+            this.sequenceNumber = sequenceNumber;
+            this.latency = latency;
+            this.packetLoss = packetLoss;
+        }
+        public NetworkHop(IPAddress ipv4Address, int sequenceNumber, int latency, int packetLoss, NetworkHop previousNode)
+        {
+            this.ipv4Address = ipv4Address;
+            this.sequenceNumber = sequenceNumber;
+            this.latency = latency;
+            this.packetLoss = packetLoss;
+            this.previousNode = previousNode;
+        }
+
+        ~NetworkHop() { }
+        
+        
+    }
+}

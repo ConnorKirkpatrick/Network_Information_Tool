d9ca074e75ac098d8a6e1c78234443525810b7e2
Connor <ConnorKirkpatrick3@gmail.com>
2022-08-14 00:00:15 +0100

    Added Average, min and max latency fields. Altered constructor to suit the usecase in NetworkMethods.cs

--- NetworkingMethods/NetworkHop.cs
+++ NetworkingMethods/NetworkHop.cs
@@ -7,7 +7,12 @@ namespace Network_Tool.NetworkingMethods
         private IPAddress ipv4Address;
         private int sequenceNumber;
         private int latency;
+        private int averageLatency;
+        private int minLatency;
+        private int maxLatency;
         private int packetLoss;
+        private int minLoss;
+        private int maxLoss;
         private NetworkHop previousNode = null;
         private NetworkHop nextNode = null;
         public IPAddress Ipv4Address
@@ -28,10 +33,40 @@ namespace Network_Tool.NetworkingMethods
             set => latency = value;
         }
 
+        public int AverageLatency
+        {
+            get => averageLatency;
+            set => averageLatency = value;
+        }
+
+        public int MinLatency
+        {
+            get => minLatency;
+            set => minLatency = value;
+        }
+
+        public int MaxLatency
+        {
+            get => maxLatency;
+            set => maxLatency = value;
+        }
+        
         public int PacketLoss
         {
             get => packetLoss;
             set => packetLoss = value;
+        }
+        
+        public int MinLoss
+        {
+            get => minLoss;
+            set => minLoss = value;
+        }
+
+        public int MaxLoss
+        {
+            get => maxLoss;
+            set => maxLoss = value;
         }
         
         public NetworkHop PreviousNode
@@ -49,12 +84,18 @@ namespace Network_Tool.NetworkingMethods
         public NetworkHop()
         { }
 
-        public NetworkHop(IPAddress ipv4Address, int sequenceNumber, int latency, int packetLoss)
+        public NetworkHop(IPAddress ipv4Address, int sequenceNumber, int latency,int packetLoss)
         {
             this.ipv4Address = ipv4Address;
             this.sequenceNumber = sequenceNumber;
             this.latency = latency;
             this.packetLoss = packetLoss;
+
+            this.averageLatency = latency;
+            this.minLatency = latency;
+            this.maxLatency = latency;
+            this.minLoss = packetLoss;
+            this.maxLoss = packetLoss;
         }
         public NetworkHop(IPAddress ipv4Address, int sequenceNumber, int latency, int packetLoss, NetworkHop previousNode)
         {
@@ -63,10 +104,13 @@ namespace Network_Tool.NetworkingMethods
             this.latency = latency;
             this.packetLoss = packetLoss;
             this.previousNode = previousNode;
+            
+            this.averageLatency = latency;
+            this.minLatency = latency;
+            this.maxLatency = latency;
+            this.minLoss = packetLoss;
+            this.maxLoss = packetLoss;
         }
-
         ~NetworkHop() { }
-        
-        
     }
 }
